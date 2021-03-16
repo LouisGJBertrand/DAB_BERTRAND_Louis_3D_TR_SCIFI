0803a892e69aacacfa45a9487e9c8fb1ef65b283
Louis BERTRAND <contact@louisbertrand.studio>
2021-03-16 11:18:16 +0100

    created prefabs and scripts

---
+++ Assets/SCRIPTS/GameCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCollider : MonoBehaviour
+{
+
+    public GameObject CubeCollider = null;
+
+    [SerializeField]
+    private Vector3 colliderSize;
+    public Vector3 ColliderSize
+    {
+        get { return colliderSize; }
+        set { CubeCollider.transform.localScale = ColliderSize; colliderSize = ColliderSize; }
+    }
+
+}
