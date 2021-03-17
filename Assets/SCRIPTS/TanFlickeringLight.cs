ac780d6b665a79ce116c2913013cc51e0382b0af
Louis BERTRAND <contact@louisbertrand.studio>
2021-03-17 14:55:48 +0100

    save work

---
+++ Assets/SCRIPTS/TanFlickeringLight.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TanFlickeringLight : MonoBehaviour
+{
+    public Light Light;
+    public float FlickeringChance = 0.01f;
+    public bool IsFlickeringInverted = true;
+
+    public float MaxLightIntensity = 1;
+    public float MinLightIntensity = 0;
+
+    public float flicker = 0;
+    public float flickerSpeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Light.intensity = (float)Math.Tan(flicker) * MaxLightIntensity + MinLightIntensity;
+        flicker = (float)((flicker + Time.deltaTime * flickerSpeed) % Math.PI);
+
+    }
+}
